da630584292ca5db5463f20f5b6844e29888f731
Hein Smith <thiswillgenspam@heinsmith.com>
2017-12-12 16:45:05 +0200

    Initial addition of Cic Resource to provide rest access.

---
+++ src/main/java/com/heinsmith/digitalplatoon/web/rest/CicResource.java
@@ -0,0 +1,24 @@
+package com.heinsmith.digitalplatoon.web.rest;
+
+import com.heinsmith.digitalplatoon.entities.Cic;
+import com.heinsmith.digitalplatoon.services.CicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cic")
+public class CicResource {
+
+    @Autowired
+    private CicService cicService;
+
+    @PostMapping
+    public Cic create(@RequestBody Cic cic) {
+        return cicService.saveCic(cic);
+    }
+
+    @GetMapping("/{cicId}")
+    public Cic get(@PathVariable Long cicId) {
+        return cicService.getCicById(cicId);
+    }
+}
