a9b29d9ce3ce50299028600e63b0ee922632e4e2
Hein Smith <thiswillgenspam@heinsmith.com>
2017-12-12 16:08:16 +0200

    Added component scanning of com.heinsmith.digitalplatoon packages.

--- src/main/java/com/heinsmith/digitalplatoon/DigitalplatoonApplication.java
+++ src/main/java/com/heinsmith/digitalplatoon/DigitalplatoonApplication.java
@@ -2,8 +2,10 @@ package com.heinsmith.digitalplatoon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.heinsmith.digitalplatoon")
 public class DigitalplatoonApplication {
 
 	public static void main(String[] args) {
