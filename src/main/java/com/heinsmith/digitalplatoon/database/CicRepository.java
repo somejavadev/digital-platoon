248c0c8c632324c6c8a84dae7aadaaf96f55e217
Hein Smith <thiswillgenspam@heinsmith.com>
2017-12-12 17:10:40 +0200

    Added repository annotation to the CicRepository.

--- src/main/java/com/heinsmith/digitalplatoon/database/CicRepository.java
+++ src/main/java/com/heinsmith/digitalplatoon/database/CicRepository.java
@@ -2,6 +2,8 @@ package com.heinsmith.digitalplatoon.database;
 
 import com.heinsmith.digitalplatoon.entities.Cic;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CicRepository extends CrudRepository<Cic, Long> {
 }
