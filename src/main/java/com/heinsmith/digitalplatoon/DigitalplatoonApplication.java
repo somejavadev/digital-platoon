5479b6450aa703f6d5ea2850f28bd9c68d8d5161
Hein Smith <thiswillgenspam@heinsmith.com>
2017-12-12 16:37:22 +0200

    Enabled JPA repositories for the spring boot app.

--- src/main/java/com/heinsmith/digitalplatoon/DigitalplatoonApplication.java
+++ src/main/java/com/heinsmith/digitalplatoon/DigitalplatoonApplication.java
@@ -3,12 +3,14 @@ package com.heinsmith.digitalplatoon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.heinsmith.digitalplatoon")
+@EnableJpaRepositories(basePackages = "com.heinsmith.digitalplatoon.entities")
 public class DigitalplatoonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DigitalplatoonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalplatoonApplication.class, args);
+    }
 }
