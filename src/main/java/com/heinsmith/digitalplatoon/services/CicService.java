008ed47f209d379fc87a94605e3f83fc8087fce8
Hein Smith <thiswillgenspam@heinsmith.com>
2017-12-12 16:41:39 +0200

    Added initial cic service (business layer), currently only calling database layer directly (passthrough).

---
+++ src/main/java/com/heinsmith/digitalplatoon/services/CicService.java
@@ -0,0 +1,9 @@
+package com.heinsmith.digitalplatoon.services;
+
+import com.heinsmith.digitalplatoon.entities.Cic;
+
+public interface CicService {
+
+    Cic getCicById(Long id);
+    Cic addCic(Cic cic);
+}
---
+++ src/main/java/com/heinsmith/digitalplatoon/services/impl/CicServiceImpl.java
@@ -0,0 +1,24 @@
+package com.heinsmith.digitalplatoon.services.impl;
+
+import com.heinsmith.digitalplatoon.database.CicRepository;
+import com.heinsmith.digitalplatoon.entities.Cic;
+import com.heinsmith.digitalplatoon.services.CicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CicServiceImpl implements CicService {
+
+    @Autowired
+    private CicRepository cicRepository;
+
+    @Override
+    public Cic getCicById(Long id) {
+        return cicRepository.findOne(id);
+    }
+
+    @Override
+    public Cic addCic(Cic cic) {
+        return cicRepository.save(cic);
+    }
+}
