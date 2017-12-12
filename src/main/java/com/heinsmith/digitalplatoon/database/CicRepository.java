a8a972fae7b2d2691db91cb7a60a0ceb483fe5a7
Hein Smith <thiswillgenspam@heinsmith.com>
2017-12-12 16:36:37 +0200

    Added initial Cic JPA repository.

---
+++ src/main/java/com/heinsmith/digitalplatoon/database/CicRepository.java
@@ -0,0 +1,7 @@
+package com.heinsmith.digitalplatoon.database;
+
+import com.heinsmith.digitalplatoon.entities.Cic;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CicRepository extends CrudRepository<Cic, Long> {
+}
