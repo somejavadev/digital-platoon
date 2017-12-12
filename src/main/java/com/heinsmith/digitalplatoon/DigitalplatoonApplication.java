e15fe376d6706aa71cc8d65fc83d79720267004e
Hein Smith <thiswillgenspam@heinsmith.com>
2017-12-12 12:53:01 +0200

    Initial check in of spring initilizer project.

---
+++ src/main/java/com/heinsmith/digitalplatoon/DigitalplatoonApplication.java
@@ -0,0 +1,12 @@
+package com.heinsmith.digitalplatoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalplatoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalplatoonApplication.class, args);
+	}
+}
